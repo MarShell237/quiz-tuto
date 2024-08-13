8da0ef8f4937bf32418108cf602932d334f7e3dc
MarShell237 <djiofackjason57@gmail.com>
2024-08-13 12:56:01 +0100

    debut de tuto

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>     
+    {{ quiz }}  
+  </div>
+</template>
+
+<script setup>
+  import {ref,onMounted} from 'vue'
+  const quiz = ref(null);
+  const state = ref('loading')
+
+onMounted(()=>{
+  fetch('./public/quiz.json')
+  .then(response => {
+    if(response.ok){
+      return response.json()
+      
+    }
+    throw new Error('impossible de recuperer le json')
+  })    
+  .then(data => {
+    quiz.value = data;
+    state.value = 'idle'
+  })
+})
+</script>
+
+<style scoped>
+
+</style>
